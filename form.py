486a09c3e2fcfbad7d449b29c724c62151dd5732
very1fake <very1fake.coder@gmail.com>
2022-04-04 16:00:15 +0300

    Questions logger

--- form.py
+++ form.py
@@ -1,10 +1,27 @@
+from pathlib import Path
 import re
-import pdb
+import json
+import atexit
+from time import time
 
 from bottle import post, request
 
-# Questions storage
-questions = {}
+# Log path
+LOG_PATH = "./questions.log.json"
+# Questions log storage
+questions = []
+
+# Load log if exists
+if Path(LOG_PATH).is_file():
+    with open(LOG_PATH, "r") as log:
+        questions = json.load(log)
+
+# Exit handler. Used to save log to `questions.log.json`
+def exit_log_handler():
+    with open(LOG_PATH, "w+") as log:
+        json.dump(questions, log, indent=4)
+
+atexit.register(exit_log_handler)
 
 # Short regex of IETF RFC 5321 4.5.3.1
 email_regex = re.compile(r"^((?!\.)[a-z0-9.]{1,64}(?<!\.))@((?!-)[a-z0-9-]{1,63}(?<!-)\.)+[a-z]{2,6}$")
@@ -36,8 +53,13 @@ def form():
         elif email_regex.match(email) is None or len(email) > 254:
             err = "Invalid email address"
 
-    questions[email] = question
-    pdb.set_trace()
+    # Appending to log (with timestamp)
+    questions.append({
+        "question": question,
+        "from": email,
+        "timestamp": int(time()),
+    })
+
     return (
         f'Thanks for your question! The answer will be sent to your email "{email}".'
         if err is None else
